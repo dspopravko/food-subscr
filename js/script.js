9eb197fb48506c6c321ad730740cca607acca9cc
Demid Popravko <demid1498@gmail.com>
2022-07-06 13:14:09 +0300

    Minor fixes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabContents = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('.show', 'fade')
+        tabContents.forEach(tabContent => {
+            tabContent.classList.add('hide');
+            tabContent.classList.remove('show', 'fade')
         });
 
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active')
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active')
         })
             
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
+        tabContents[i].classList.add('show', 'fade');
+        tabContents[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active')
     }
 
-
-    hideTabContent(); showTabContent();
+    hideTabContent();
+    showTabContent();
 
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
