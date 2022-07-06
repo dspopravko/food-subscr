13914c432c74bb72e76cb3c94bf535f8fbcd779b
Demid Popravko <demid1498@gmail.com>
2022-07-06 15:49:26 +0300

    Countdown timer

--- js/script.js
+++ js/script.js
@@ -37,4 +37,55 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+    //Timer
+    const deadline = '2022-08-07'
+
+    function getTimeRemaining(endtime) {
+        const remainTime = Date.parse(endtime) - Date.parse(new Date());
+              days = Math.floor(remainTime/(1000*60*60*24));
+              hours = Math.floor(remainTime/(1000*60*60) % 24);
+              minutes = Math.floor((remainTime/1000/60)%60);
+              seconds = Math.floor((remainTime/1000)%60)
+
+        return {
+            'total': remainTime,
+            days,
+            hours,
+            minutes,
+            seconds
+        }
+    }
+
+    function addZero(num) {
+        if (num >= 0 && num < 10)
+            return `0${num}`
+        else return num
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock,1000)
+
+        updateClock();
+              
+        function updateClock() {
+            const remainTime = getTimeRemaining(endtime);
+
+            days.innerHTML = addZero(remainTime.days);
+            hours.innerHTML = addZero(remainTime.hours);
+            minutes.innerHTML = addZero(remainTime.minutes);
+            seconds.innerHTML = addZero(remainTime.seconds)
+
+            if (remainTime.total <= 0) {
+                clearInterval(timeInterval)
+            }
+        }
+    }
+    setClock('.timer', deadline);
+    
 })
